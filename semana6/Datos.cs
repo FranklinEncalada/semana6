234299344dbd1ee1a23e47bc73210d83c153a3a7
master <master@SMLAB408PC04_22>
2022-05-14 10:39:25 -0500

    Agregar archivos de proyecto.

---
+++ semana6/Datos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace semana6
+{
+    class Datos
+    {
+
+
+       public int codigo{ get; set; }
+        public int Nombre { get; set; }
+        public int Apellido { get; set; }
+        public int edad { get; set; }
+
+
+
+    }
+}
---
+++ semana6/conexiondb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace semana6
+{
+    class conexiondb
+    {
+
+    }
+}
